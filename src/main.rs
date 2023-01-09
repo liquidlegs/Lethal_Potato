3b99de2c390e20b698d3c8015f3742edccca8e63
liquidlegs <makurosofuto0uindo@gmail.com>
2023-01-09 17:20:28 +1030

    Added missing '-h' flag

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
   let display: Vec<String> = std::env::args().collect();
   if display.len() > 1 {
     match display[1].as_str() {
-      "--help" => {
+      "--help" | "-h" => {
         display_help(display[0].clone());
       }
 
