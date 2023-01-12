4bb4cc267a01aac2c4f8ff6ec2a52060c0c11bc8
liquidlegs <makurosofuto0uindo@gmail.com>
2023-01-13 00:52:23 +1030

    added banner grabbing and banner length customization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod arguments;
-use arguments::{Arguments, ArgumentSettings, display_help, FileOutput};
+use arguments::{Arguments, ArgumentSettings, display_help};
 use clap::Parser;
 
 fn main() {
@@ -7,9 +7,6 @@ fn main() {
   if display.len() > 1 {
     match display[1].as_str() {
       "--help" | "-h" => {
-        // let current_dir = std::env::current_dir().unwrap();
-        // println!("{}", current_dir.into_os_string().into_string().unwrap());
-
         display_help(display[0].clone());
       }
 
@@ -20,7 +17,7 @@ fn main() {
   let mut args = Arguments::parse();
   let mut settings = ArgumentSettings::new();
   
-  if let Some(s) = args.output.clone() {
+  if let Some(_) = args.output.clone() {
     settings.is_valid_output_path = args.check_valid_directory();
   }
   
